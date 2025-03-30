3e30e90eaf5b545c6a70e5b4f7a3a46771fbf77f
kdusi <kdusi@wisc.edu>
2025-03-30 05:54:44 -0500

    started neural net

---
+++ src/neuralnet_player.rs
@@ -0,0 +1,14 @@
+use crate::game_state::GameState;
+use crate::player::Player;
+
+pub struct NeuralNetPlayer;
+
+impl NeuralNetPlayer {
+
+
+}
+impl Player for NeuralNetPlayer {
+    fn make_move(&mut self, game_state: &mut GameState) {
+        // Implement the logic to make a move using the neural network
+    }
+}
